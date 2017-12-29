7265f46408bf8dc1b4c4ed43af018b5060060230
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-12-29 14:30:34 -0500

    initial push with nodejs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
+var port = Number(process.env.PORT || 3000);
+connect().use(serveStatic(__dirname)).listen(port, function(){
     console.log('Server running on 8080...');
 });
