05dee2ab7ccc114152fa3f25a96858a93f264e2f
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-12-31 13:25:55 -0500

    indent and clean

--- js/events.js
+++ js/events.js
@@ -105,34 +105,6 @@
         };
 
         /* Team*/
-        $(".corevision").click(function() {
-        $( "#corevision" ).toggle();
-        $( "#coregoals" ).hide();
-        $( "#corevals" ).hide();
-        $( ".corevision" ).toggleClass("corevaluesactive");
-        $( ".coregoals" ).removeClass("corevaluesactive");
-        $( ".corevals" ).removeClass("corevaluesactive");
-        $(window).scrollTop($('#corevision').offset().top - 300).scrollLeft($('#corevision').offset().left);
-      });
-      $(".coregoals").click(function() {
-        $( "#coregoals" ).toggle();
-        $( "#corevision" ).hide();
-        $( "#corevals" ).hide();
-        $( ".corevision" ).removeClass("corevaluesactive");
-        $( ".coregoals" ).toggleClass("corevaluesactive");
-        $( ".corevals" ).removeClass("corevaluesactive");
-        $(window).scrollTop($('#coregoals').offset().top - 300).scrollLeft($('#coregoals').offset().left);
-      });
-      $(".corevals").click(function() {
-        $( "#corevals" ).toggle();
-        $( "#coregoals" ).hide();
-        $( "#corevision" ).hide();
-        $( ".corevision" ).removeClass("corevaluesactive");
-        $( ".coregoals" ).removeClass("corevaluesactive");
-        $( ".corevals" ).toggleClass("corevaluesactive");
-        $(window).scrollTop($('#corevals').offset().top - 300).scrollLeft($('#corevals').offset().left);
-      });
-
       $( ".at-column" ).mouseleave(function() {
         //console.log("mouseout");
         $(this).find(".at-social").hide();
