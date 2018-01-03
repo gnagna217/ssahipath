aa3e95fba88248e04731738f34170d375ca1cc9f
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-03 10:46:06 -0500

    add error messages

--- js/validate.js
+++ js/validate.js
@@ -74,7 +74,7 @@ console.log(values);
       $("#result").html("The code you entered does not match. Please enter the code as displayed");
       $("#result").show();
     }
-    /*console.log();
+    console.log();
     ajaxRequest= $.ajax({
             url: "/js/test.php",
             type: "post",
@@ -85,12 +85,12 @@ console.log(values);
           // show successfully for submit message
           $("#result").html(response);
      });
-*/
+
      /* On failure of request this function will be called  */
- /*    ajaxRequest.fail(function (){
+     ajaxRequest.fail(function (){
        // show error
        $("#result").html('There was an error in processing the request. Please try again.');
-     });*/
+     });
   }
 
   $("form[name='volunteer']").validate({
